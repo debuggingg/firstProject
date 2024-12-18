cf7f3e7c3c10999b82b2f18d61ee6144db08ace0
debuggingg <beom.kb@gmail.com>
2024-12-18 11:17:23 +0900

    ㅁ
    ㅁ

--- webproject/src/main/java/springboot/webproject/repository/TestRepository.java
+++
@@ -1,8 +0,0 @@
-package springboot.webproject.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import springboot.webproject.entity.TestEntity;
-
-@Repository
-public interface TestRepository extends JpaRepository<TestEntity, Long> {
-}
